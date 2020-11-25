65dedbfea278a22b221f4a90bac3089cc5cb3548
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-11-25 17:26:41 -0300

    test: ajuste para considerar novo argumento da CLI

--- tests/command-line-ard-parser.spec.js
+++ tests/command-line-ard-parser.spec.js
@@ -5,13 +5,14 @@ describe('command line args parser', () => {
     // Given
 
     // When
-    const { app, gateway, breakBuild, path } = commandLineArgsParser.parse();
+    const { app, gateway, breakBuild, path, components } = commandLineArgsParser.parse();
 
     // Then
     expect(app).toBe(undefined);
     expect(gateway).toBe(100);
     expect(path).toBe('src');
     expect(breakBuild).toBe(false);
+    expect(components).toBe(undefined);
   })
 
   it('parses given values', () => {
@@ -20,14 +21,16 @@ describe('command line args parser', () => {
     process.argv.push('--gateway', '70');
     process.argv.push('--src', 'src/app');
     process.argv.push('--break-build');
+    process.argv.push('--components=wiz-privacy');
 
     // When
-    const { app, gateway, breakBuild, path } = commandLineArgsParser.parse();
+    const { app, gateway, breakBuild, path, components } = commandLineArgsParser.parse();
 
     // Then
     expect(app).toBe('my-app');
     expect(gateway).toBe(70);
     expect(path).toBe('src/app');
     expect(breakBuild).toBe(true);
+    expect(components).toBe('wiz-privacy');
   })
 });
